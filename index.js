adb3d9cdba910d49cd9f9081e939e0dd088b40c6
Douglas Raith <draith2001@gmail.com>
2017-12-31 16:11:01 +0000

    Updated index and html files

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
-var omx = require('omx-manager');
 
 var handle = {};
 handle["/start"] = requestHandlers.start;
 handle["/stop"] = requestHandlers.stop;
-handle["/play"] = requestHandlers.play;
-handle["/playvid"] = requestHandlers.playvid;
-handle["/playdir"] = requestHandlers.playdir;
-handle["/pause"] = requestHandlers.pause;
-handle["/prevch"] = requestHandlers.prevch;
-handle["/nextch"] = requestHandlers.nextch;
-handle["/back30s"] = requestHandlers.back30s;
-handle["/fwd30s"] = requestHandlers.fwd30s;
-handle["/back10m"] = requestHandlers.back10m;
-handle["/fwd10m"] = requestHandlers.fwd10m;
 
 server.start(router.route, handle);
-//omx.enableHangingHandler();
